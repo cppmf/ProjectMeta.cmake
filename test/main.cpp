874db791230f7b3e7b76cda3b5c46e3f93743c84
gennesseaux <jocelyn@gennesseaux.fr>
2021-10-29 18:45:33 +0200

    test

--- test/main.cpp
+++ test/main.cpp
@@ -7,10 +7,10 @@ int main(int argc, char* argv[])
 {
 #if defined (META_PROJECT_NAME) && defined (META_VERSION)
     std::cout << "This is " << META_PROJECT_NAME << " version " << META_VERSION << std::endl;
+    return 0;
 #else
     std::cout << "Project meta data not found." << std::endl;
     std::cout << "Check the project declaration." << std::endl;
+    return 1;
 #endif
-
-    return 0;
 }
